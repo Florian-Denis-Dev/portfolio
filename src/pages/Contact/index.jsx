7379e38054aa776f45faf489221c9c1baf3a9b6b
florian denis <136095872+Florian-Denis-Dev@users.noreply.github.com>
2024-06-07 17:28:06 +0200

    update contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 
 
 function Contact(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      window.open(`mailto:floriandenisfdpro@gmail.com?subject=Message de ${name} (${email})&body=${message}`);
+    };
     return(
         <div className="contact">
-            <div className="about-regles">
+            <div>
+                <h4>Contact me</h4>
+                <a href="https://www.instagram.com/florian.denis.dev/">instagram</a>
             </div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nom :
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+                </label>
+                <label>
+                    Email :
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </label>
+                <label>
+                    Message :
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)} required />
+                </label>
+                <button type="submit">Envoyer</button>
+            </form>
         </div>
     )
 }
