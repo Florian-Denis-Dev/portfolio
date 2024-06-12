51cda33f281c2d544ad74fbe3272108f50346c08
florian denis <136095872+Florian-Denis-Dev@users.noreply.github.com>
2024-06-12 12:52:27 +0200

    modif ++
    switch logic works
    change css about
    delete mun css

--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import P3 from "../../assets/p3.png";
-import P6 from "../../assets/p6.png";
-import P7 from "../../assets/p7.png";
+import p3 from "../../assets/p3.png";
+import p6 from "../../assets/p6.png";
+import p7 from "../../assets/p7.png";
+
 
 function Works({ projects }) {
     return (
@@ -11,33 +12,17 @@ function Works({ projects }) {
                 <h2>My projects</h2>
             </div>
             <div className="works">
-                <div>
-                    {projects.map((project, index) => {
-                        let imgSrc;
-                        let altText;
-                        if (index === 0) {
-                            imgSrc = P3;
-                            altText = "work frontend for sophie bluel";
-                        } else if (index === 1) {
-                            imgSrc = P6;
-                            altText = "work frontend for kasa";
-                        } else {
-                            imgSrc = P7;
-                            altText = "work backend for mon vieux grimmoire";
-                        }
-                        return (
-                            <fig key={project.id}>
-                                <Link to={`/works/${project.id}`}>
-                                    <img src={imgSrc} alt={altText}></img>
-                                </Link>
-                                <figcaption>{project.title}</figcaption>
-                            </fig>
-                        );
-                    })}
-                </div>
+                {projects.map((project) => (
+                    <figure key={project.id}>
+                        <Link to={`/works/${project.id}`}>
+                            <img src={project.id === "project1" ? p3 : project.id === "project2" ? p6 : p7} alt={`work ${project.title}`} />
+                        </Link>
+                        <figcaption>{project.title}</figcaption>
+                    </figure>
+                ))}
             </div>
         </>
-    )
+    );
 }
 
 export default Works;
